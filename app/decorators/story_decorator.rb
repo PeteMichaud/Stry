df4a0e75611f28bfe8254fbbba6412333114986c
Pete Michaud <michaudp@gmail.com>
2012-09-27 10:42:34 -0500

    added story decorator

---
+++ app/decorators/story_decorator.rb
@@ -0,0 +1,9 @@
+class StoryDecorator < Draper::Base
+  decorates :story
+  decorates_association :scenes
+
+  def table_of_contents
+    h.render :partial => 'stories/table_of_contents', :locals => { story: model }
+  end
+
+end
