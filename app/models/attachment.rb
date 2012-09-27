b23c2f2e16f0ecabc08cc6bcb24bccc7adbbb478
Pete Michaud <michaudp@gmail.com>
2012-09-27 14:42:31 -0500

    attachment model makes sure the attachment is an image before trying to perform image processing on it

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -2,10 +2,13 @@ class Attachment < ActiveRecord::Base
   attr_accessible :file, :caption, :sequence, :block_id
   has_attached_file :file, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 
+  before_post_process :new_file_is_image?
+
   belongs_to :block
 
   validates :block, :presence => true
   validates :file, :attachment_presence => true
+
 
   def is_image?
     is_type? %w(jpg jpeg gif png tif tiff bmp)
@@ -24,5 +27,10 @@ class Attachment < ActiveRecord::Base
   def is_type? legal
     legal.include? File.extname(file.path)[1..4] unless file.path.nil?
   end
+
+  def new_file_is_image?
+    %w(jpg jpeg gif png tif tiff bmp).map{|ext| "image/#{ext}" }.include?(file_content_type)
+  end
+
 
 end
