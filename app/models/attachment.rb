c277f827a684204acf912f4139152f85e20e8069
Pete Michaud <michaudp@gmail.com>
2012-09-27 09:36:03 -0500

    attachment model has check for image, audio, and video, and can render based on detected type

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -7,4 +7,34 @@ class Attachment < ActiveRecord::Base
   validates :block, :presence => true
   validates :file, :attachment_presence => true
 
+  #Presents the file in html based on it's file type
+  def present
+    if is_image?
+      render :partial => 'attachments/types/image', :locals => { :image => file }
+    elsif is_audio?
+      render :partial => 'attachments/types/audio', :locals => { :sound => file }
+    elsif is_video?
+      render :partial => 'attachments/types/video', :locals => { :video => file }
+    else
+      render :partial => 'attachments/types/generic', :locals => { :file => file }
+    end
+
+  end
+
+  def is_image?
+    is_type? %w(jpg jpeg gif png tif tiff bmp)
+  end
+
+  def is_audio?
+    is_type? %w(mp3 m4a wav)
+  end
+
+  def is_video?
+    is_type? %w(mpg mpeg avi flv mov mp4 wmv)
+  end
+
+  def is_type? legal
+    legal.include? File.extname(file.path)
+  end
+
 end
