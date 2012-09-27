25794dc1df79f319860f4debcb6acc59c369dc49
Pete Michaud <michaudp@gmail.com>
2012-09-27 15:09:38 -0500

    story decorator derives from application decorator and loses editable

--- app/decorators/story_decorator.rb
+++ app/decorators/story_decorator.rb
@@ -1,17 +1,9 @@
-class StoryDecorator < Draper::Base
+class StoryDecorator < ApplicationDecorator
   decorates :story
   decorates_association :scenes
 
   def table_of_contents
     h.render :partial => 'stories/table_of_contents', :locals => { story: model }
-  end
-
-  def editable field
-    h.render :partial => 'shared/editable_field', :locals => {
-        type: 'story',
-        prop: field.to_s,
-        object: self
-    }
   end
 
 end
