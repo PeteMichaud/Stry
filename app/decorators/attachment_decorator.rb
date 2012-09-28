d5669550d77df15809f2ca7ad3fa4875a7644d1a
Pete Michaud <michaudp@gmail.com>
2012-09-28 12:40:26 -0500

    all the attachment partials get the attachment object instead of just the file

--- app/decorators/attachment_decorator.rb
+++ app/decorators/attachment_decorator.rb
@@ -3,13 +3,13 @@ class AttachmentDecorator < ApplicationDecorator
 
   def render
     if model.is_image?
-      h.render :partial => 'attachments/types/image', :locals => { :image => file }
+      h.render :partial => 'attachments/types/image', :locals => { :attachment => self }
     elsif model.is_audio?
       h.render :partial => 'attachments/types/audio', :locals => { :attachment => self }
     elsif model.is_video?
       h.render :partial => 'attachments/types/video', :locals => { :attachment => self }
     else
-      h.render :partial => 'attachments/types/file', :locals => { :file => file }
+      h.render :partial => 'attachments/types/file', :locals => { :attachment => self }
     end
   end
 end
