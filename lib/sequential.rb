e0f18ee78241b2df799e6a8ba5332defb30b3c27
Pete Michaud <michaudp@gmail.com>
2013-02-07 16:33:22 -0600

    sequential now works with AR

--- lib/sequential.rb
+++ lib/sequential.rb
@@ -2,13 +2,15 @@ module Sequential
 
   def insert_object collection, obj, after_id = nil
     raise "Cannot Insert Nil Object" unless obj.present?
-    previous_obj = obj.class.name.constantize.find after_id unless after_id.blank?
+    previous_obj = obj.class.find after_id unless after_id.blank?
     obj.sequence = previous_obj.present? ? previous_obj.sequence + 1 : 0
     collection.where("sequence >= ?", obj.sequence).each do |obj|
       obj.sequence += 1
       obj.save
     end
+    obj.send("#{self.class.name.downcase}_id=", self.id)
     obj.save
+    self.send(collection.klass.name.downcase.pluralize).reload
   end
 
   def remove_object collection, obj
