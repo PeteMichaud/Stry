910ac431513924604c0d29c34156c3eb78ec77cc
Pete Michaud <michaudp@gmail.com>
2013-02-07 16:34:29 -0600

    addd new_blank_by author method to story, and updated insert_object call with scope

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,6 @@
 class Story < ActiveRecord::Base
   include Sequential
-  attr_accessible :title, :description, :author_id
+  attr_accessible :title, :description, :author_id, :author
 
   has_many :scenes, :order => "sequence ASC", :dependent => :destroy
   belongs_to :author, :class_name => User
@@ -8,11 +8,22 @@ class Story < ActiveRecord::Base
   validates :author, :presence => true
 
   def insert scene, after_id = nil
-    insert_object scenes, scene, after_id
+    insert_object scenes.scoped, scene, after_id
   end
 
   def remove scene
     remove_object scenes, scene
+  end
+
+  def self.new_blank_by author
+    story = Story.create({title: '', author: author})
+
+    story.insert Scene.new
+    story.scenes.first.insert Block.new
+
+    story.save!
+
+    story
   end
 
 end
