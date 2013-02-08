8ecc3682be829439c69fd18ada5fef0eb319205e
Pete Michaud <michaudp@gmail.com>
2013-02-07 21:14:21 -0600

    updated t owor kwith new editor namespace

--- app/decorators/attachment_decorator.rb
+++ app/decorators/attachment_decorator.rb
@@ -3,13 +3,13 @@ class AttachmentDecorator < ApplicationDecorator
 
   def render
     if model.is_image?
-      h.render :partial => 'attachments/types/image', :locals => { :attachment => self }
+      h.render :partial => 'editor/attachments/types/image', :locals => { :attachment => self }
     elsif model.is_audio?
-      h.render :partial => 'attachments/types/audio', :locals => { :attachment => self }
+      h.render :partial => 'editor/attachments/types/audio', :locals => { :attachment => self }
     elsif model.is_video?
-      h.render :partial => 'attachments/types/video', :locals => { :attachment => self }
+      h.render :partial => 'editor/attachments/types/video', :locals => { :attachment => self }
     else
-      h.render :partial => 'attachments/types/file', :locals => { :attachment => self }
+      h.render :partial => 'editor/attachments/types/file', :locals => { :attachment => self }
     end
   end
 end
